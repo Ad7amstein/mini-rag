9d544515d9132d5fc074f4f454c88090526eed86
Ad7amstein <adham32003200@gmail.com>
2025-08-11 23:15:47 +0300

    feat: include routes in main file

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import os
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from routes import base
+
+load_dotenv(".env", override=True, verbose=True)
 
 app = FastAPI()
-
-
-@app.get("/welcome")
-def welcome():
-    return {"messge": "Hello, FastAPI!"}
+app.include_router(base.base_router)
 
 
 def main():
     """Entry Point for the Program."""
-    print(
-        f"Welcome from `{os.path.basename(__file__).split('.')[0]}` Module. Nothing to do ^_____^!"
-    )
+    print(f"Welcome from `{os.path.basename(__file__).split('.')[0]}` Module.")
 
 
 if __name__ == "__main__":
