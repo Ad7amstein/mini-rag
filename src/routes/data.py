613ea66c21440851e58d1c136745e8214786e949
Ad7amstein <adham32003200@gmail.com>
2025-08-12 13:02:35 +0300

    feat: enhance upload_data endpoint to return JSON responses with status codes

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,5 +1,6 @@
 import os
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, UploadFile, status
+from fastapi.responses import JSONResponse
 from controllers import DataController
 
 data_router = APIRouter(prefix="/api/v1/data", tags=["api_v1", "data"])
@@ -9,7 +10,16 @@ data_router = APIRouter(prefix="/api/v1/data", tags=["api_v1", "data"])
 async def upload_data(project_id: str, file: UploadFile):
     is_valid, result_signal = DataController().validate_uploaded_file(file)
 
-    return {"is valid": is_valid, "signal": result_signal}
+    if not is_valid:
+        response = JSONResponse(
+            content={"signal": result_signal}, status_code=status.HTTP_400_BAD_REQUEST
+        )
+    else:
+        response = JSONResponse(
+            content={"signal": result_signal}, status_code=status.HTTP_200_OK
+        )
+
+    return response
 
 
 def main():
