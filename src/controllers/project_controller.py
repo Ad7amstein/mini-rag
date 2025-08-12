d6f44542004be6bf1aedfc6832f650659eb9ad5e
Ad7amstein <adham32003200@gmail.com>
2025-08-12 15:59:53 +0300

    feat: implement ProjectController with project path management

---
+++ src/controllers/project_controller.py
@@ -0,0 +1,24 @@
+import os
+from controllers.base_controller import BaseController
+
+
+class ProjectController(BaseController):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def get_project_path(self, project_id: str):
+        project_dir = os.path.join(self.files_dir, project_id)
+        os.makedirs(project_dir, exist_ok=True)
+
+        return project_dir
+
+
+def main():
+    """Entry Point for the Program."""
+    print(
+        f"Welcome from `{os.path.basename(__file__).split('.')[0]}` Module. Nothing to do ^_____^!"
+    )
+
+
+if __name__ == "__main__":
+    main()
