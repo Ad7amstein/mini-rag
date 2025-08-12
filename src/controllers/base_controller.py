c14562bd45788cbaba0c6575c7cd667e2672d9e5
Ad7amstein <adham32003200@gmail.com>
2025-08-12 16:00:24 +0300

    feat: enhance BaseController with random string generation and directory management

--- src/controllers/base_controller.py
+++ src/controllers/base_controller.py
@@ -1,10 +1,19 @@
 import os
+import random
 from utils.config_utils import get_settings
+import string
 
 
 class BaseController:
     def __init__(self) -> None:
         self.app_settings = get_settings()
+        self.base_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+        self.files_dir = os.path.join(self.base_dir, "assets", "files")
+
+    def generate_random_string(self, length: int = 12):
+        return "".join(
+            random.choices("".join([string.ascii_lowercase, string.digits]), k=length)
+        )
 
 
 def main():
