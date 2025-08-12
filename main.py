766a6dd66bac95ecd896643146f88288dfd594c0
Ad7amstein <adham32003200@gmail.com>
2025-08-12 11:36:42 +0300

    refactor: remove dotenv loading from main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import os
 from fastapi import FastAPI
-from dotenv import load_dotenv
 from routes import base
-
-load_dotenv(".env", override=True, verbose=True)
 
 app = FastAPI()
 app.include_router(base.base_router)
