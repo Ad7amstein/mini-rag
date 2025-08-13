461f85006c50f57aed8a33b59c189ae90c87b7da
Ad7amstein <adham32003200@gmail.com>
2025-08-13 15:28:02 +0300

    feat: add ProcessRequest class and main function in data.py for processing requests

---
+++ src/routes/schemas/data.py
@@ -0,0 +1,21 @@
+import os
+from pydantic import BaseModel
+from typing import Optional
+
+
+class ProcessRequest(BaseModel):
+    file_id: str
+    chunk_size: Optional[int] = 100
+    overlap_size: Optional[int] = 20
+    do_reset: Optional[int] = 0
+
+
+def main():
+    """Entry Point for the Program."""
+    print(
+        f"Welcome from `{os.path.basename(__file__).split('.')[0]}` Module. Nothing to do ^_____^!"
+    )
+
+
+if __name__ == "__main__":
+    main()
