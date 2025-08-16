28bf1ac616ff1f54732a2bde0a1ce35c5ec313e6
Ad7amstein <adham32003200@gmail.com>
2025-08-16 10:31:54 +0300

    fix: replace ResponseSignal with ResponseSignalEnum

--- src/controllers/data_controller.py
+++ src/controllers/data_controller.py
@@ -3,7 +3,7 @@ import re
 from fastapi import UploadFile
 from controllers.base_controller import BaseController
 from controllers.project_controller import ProjectController
-from models import ResponseSignal
+from models import ResponseSignalEnum
 
 
 class DataController(BaseController):
@@ -13,11 +13,11 @@ class DataController(BaseController):
 
     def validate_uploaded_file(self, file: UploadFile):
         if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
-            return False, ResponseSignal.FILE_TYPE_NOT_SUPPORTED.value
+            return False, ResponseSignalEnum.FILE_TYPE_NOT_SUPPORTED.value
         if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:  # type: ignore
-            return False, ResponseSignal.FILE_SIZE_EXCEEDED.value
+            return False, ResponseSignalEnum.FILE_SIZE_EXCEEDED.value
 
-        return True, ResponseSignal.FILE_UPLOAD_SUCCESS.value
+        return True, ResponseSignalEnum.FILE_UPLOAD_SUCCESS.value
 
     def clean_file_name(self, file_name: str):
         cleaned_file_name = re.sub(r"[^\w.]", "", file_name)
