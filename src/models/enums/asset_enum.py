3cb6ab415c79085b6cafa5e5e752468f2e7aa9bd
Ad7amstein <adham32003200@gmail.com>
2025-08-16 10:32:57 +0300

    feat: add AssetTypeEnum class and entry point in asset_enum.py

---
+++ src/models/enums/asset_enum.py
@@ -0,0 +1,17 @@
+import os
+from enum import Enum
+
+
+class AssetTypeEnum(Enum):
+    FILE = "file"
+
+
+def main():
+    """Entry Point for the Program."""
+    print(
+        f"Welcome from `{os.path.basename(__file__).split('.')[0]}` Module. Nothing to do ^_____^!"
+    )
+
+
+if __name__ == "__main__":
+    main()
