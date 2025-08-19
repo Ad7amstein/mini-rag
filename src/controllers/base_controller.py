a367e835f64ddc7e6ddf5bd66cf355b4c2ea7a06
Ad7amstein <adham32003200@gmail.com>
2025-08-19 12:53:27 +0300

    feat: handle database path

--- src/controllers/base_controller.py
+++ src/controllers/base_controller.py
@@ -10,10 +10,17 @@ class BaseController:
         self.base_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
         self.files_dir = os.path.join(self.base_dir, "assets", "files")
 
+        self.database_dir = os.path.join(self.base_dir, "assets", "database")
+
     def generate_random_string(self, length: int = 12):
         return "".join(
             random.choices("".join([string.ascii_lowercase, string.digits]), k=length)
         )
+
+    def get_database_path(self, db_name: str):
+        database_path = os.path.join(self.database_dir, db_name)
+        os.makedirs(database_path, exist_ok=True)
+        return database_path
 
 
 def main():
