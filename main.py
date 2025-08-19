aad91ebf638258bdd3a7b48b36e7e237b30777ff
Ad7amstein <adham32003200@gmail.com>
2025-08-19 10:54:23 +0300

    feat: add logging and LLM provider initialization

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import os
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
+import logging
 from routes import base, data
 from utils import get_settings
+from stores.llm import LLMProviderFactory
+
+logger = logging.getLogger("uvicorn")
 
 
 @asynccontextmanager
@@ -13,7 +17,27 @@ async def lifespan(app: FastAPI):
     app.state.settings = settings
     app.state.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.state.db_client = app.state.mongo_conn[settings.MONGODB_DATABASE]
-    print("MongoDB connection established.")
+    logger.info("MongoDB connection established.")
+
+    llm_provider_factory = LLMProviderFactory(app.state.settings)
+    app.state.generation_client = llm_provider_factory.create(
+        app.state.settings.GENERATION_BACKEND
+    )
+
+    if app.state.generation_client is None:
+        logger.error("Error while creating generation provider")
+    app.state.generation_client.set_generation_model(  # type: ignore
+        app.state.settings.GENERATION_MODEL_ID
+    )
+
+    app.state.embedding_client = llm_provider_factory.create(
+        app.state.settings.EMBEDDING_BACKEND
+    )
+    if app.state.embedding_client is None:
+        logger.error("Error while creating embedding provider")
+    app.state.embedding_client.set_embedding_model(  # type: ignore
+        app.state.settings.EMBEDDING_MODEL_ID, app.state.settings.EMBEDDING_MODEL_SIZE
+    )
 
     yield  # The application runs here
 
