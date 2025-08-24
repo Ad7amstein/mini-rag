de5ed5f5e698bd3992561654a35b6fc0d37b1cef
Ad7amstein <adham32003200@gmail.com>
2025-08-24 19:47:31 +0300

    fix: refactor import statements to use minirag.schemas module

--- src/models/db_schemas/__init__.py
+++ src/models/db_schemas/__init__.py
@@ -1,3 +1,6 @@
-from models.db_schemas.project import Project
-from models.db_schemas.data_chunk import DataChunk, RetrievedDocument
-from models.db_schemas.asset import Asset
+from models.db_schemas.minirag.schemas import (
+    Project,
+    DataChunk,
+    RetrievedDocument,
+    Asset,
+)
