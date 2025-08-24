48f94abcc4764cc9e96d78496074967bb2764300
Ad7amstein <adham32003200@gmail.com>
2025-08-24 19:48:24 +0300

    feat: add initial schema imports in __init__.py

---
+++ src/models/db_schemas/minirag/schemas/__init__.py
@@ -0,0 +1,4 @@
+from models.db_schemas.minirag.schemas.minirag_base import SQLAlchemyBase
+from models.db_schemas.minirag.schemas.asset import Asset
+from models.db_schemas.minirag.schemas.data_chunk import DataChunk, RetrievedDocument
+from models.db_schemas.minirag.schemas.project import Project
