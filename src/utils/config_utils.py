9ac59d8f309a25cf22f7d4c05b0923ef32d4366b
Ad7amstein <adham32003200@gmail.com>
2025-08-24 19:18:15 +0300

    fix: remove unused MongoDB configuration fields from Settings class

--- src/utils/config_utils.py
+++ src/utils/config_utils.py
@@ -32,8 +32,6 @@ class Settings(BaseSettings):
     FILE_ALLOWED_TYPES: List[str] = Field(...)
     FILE_MAX_SIZE: int = Field(...)
     FILE_DEFAULT_CHUNK_SIZE: int = Field(...)
-    MONGODB_URL: str = Field(...)
-    MONGODB_DATABASE: str = Field(...)
 
     GENERATION_BACKEND: str
     EMBEDDING_BACKEND: str
