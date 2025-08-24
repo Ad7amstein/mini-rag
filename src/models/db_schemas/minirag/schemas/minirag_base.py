e460726f85b41c59ea41f7ce4b8d0dc95b6aa372
Ad7amstein <adham32003200@gmail.com>
2025-08-24 19:48:41 +0300

    feat: add minirag_base.py with SQLAlchemy base and main entry point

---
+++ src/models/db_schemas/minirag/schemas/minirag_base.py
@@ -0,0 +1,15 @@
+import os
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLAlchemyBase = declarative_base()
+
+
+def main():
+    """Entry Point for the Program."""
+    print(
+        f"Welcome from `{os.path.basename(__file__).split('.')[0]}` Module. Nothing to do ^_____^!"
+    )
+
+
+if __name__ == "__main__":
+    main()
