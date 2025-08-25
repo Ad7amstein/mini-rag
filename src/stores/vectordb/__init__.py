ae32f26e6800a5af98571290c7cc42d320d77207
Ad7amstein <adham32003200@gmail.com>
2025-08-25 20:08:26 +0300

    feat: expand imports in vectordb module to include PgVector enums

--- src/stores/vectordb/__init__.py
+++ src/stores/vectordb/__init__.py
@@ -1,3 +1,9 @@
 from stores.vectordb.vectordb_interface import VectorDBInterface
-from stores.vectordb.vectordb_enum import VectorDBEnum, DistanceMethodEnum
+from stores.vectordb.vectordb_enum import (
+    VectorDBEnum,
+    DistanceMethodEnum,
+    PgVectorDistanceMethodEnums,
+    PgVectorIndexTypeEnums,
+    PgVectorTableSchemeEnums,
+)
 from stores.vectordb.vectordb_provider_factory import VectorDBProviderFactory
