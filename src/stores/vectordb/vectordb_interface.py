82ff4807efe06edfae2b354564f944c7c63dd074
Ad7amstein <adham32003200@gmail.com>
2025-08-26 15:40:09 +0300

    feat: update VectorDBInterface methods to be asynchronous

--- src/stores/vectordb/vectordb_interface.py
+++ src/stores/vectordb/vectordb_interface.py
@@ -6,37 +6,37 @@ from models.db_schemas import RetrievedDocument
 
 class VectorDBInterface(ABC):
     @abstractmethod
-    def connect(self):
+    async def connect(self):
         pass
 
     @abstractmethod
-    def disconnect(self):
+    async def disconnect(self):
         pass
 
     @abstractmethod
-    def is_collection_existed(self, collection_name: str) -> bool:
+    async def is_collection_existed(self, collection_name: str) -> bool:
         pass
 
     @abstractmethod
-    def list_all_collections(self) -> List:
+    async def list_all_collections(self) -> List:
         pass
 
     @abstractmethod
-    def get_collection_info(self, collection_name: str) -> dict:
+    async def get_collection_info(self, collection_name: str) -> dict:
         pass
 
     @abstractmethod
-    def delete_collection(self, collection_name: str):
+    async def delete_collection(self, collection_name: str):
         pass
 
     @abstractmethod
-    def create_collection(
-        self, collection_name: str, embedding_size: int, do_resent: bool = False
+    async def create_collection(
+        self, collection_name: str, embedding_size: int, do_reset: bool = False
     ):
         pass
 
     @abstractmethod
-    def insert_one(
+    async def insert_one(
         self,
         collection_name: str,
         text: str,
@@ -47,19 +47,19 @@ class VectorDBInterface(ABC):
         pass
 
     @abstractmethod
-    def insert_many(
+    async def insert_many(
         self,
         collection_name: str,
         texts: List[str],
         vectors: List[list],
         metadatas: Optional[List[dict]] = None,
-        record_ids: Optional[List[str]] = None,
+        record_ids: Optional[List[int]] = None,
         batch_size: int = 50,
     ):
         pass
 
     @abstractmethod
-    def search_by_vector(
+    async def search_by_vector(
         self, collection_name: str, vector: list, limit: int
     ) -> List[RetrievedDocument]:
         pass
